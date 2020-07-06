86e881e3c8c5a0953f51f0b39bcfd6bb0279a9c4
Nam <nguyenxuannam511@gmail.com>
2020-07-06 17:10:22 +0700

    add search features and Display product in admin view

--- app/src/main/java/com/example/ecommerce/AdminNewOrdersActivity.java
+++ app/src/main/java/com/example/ecommerce/AdminNewOrdersActivity.java
@@ -1,10 +1,13 @@
 package com.example.ecommerce;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -50,13 +53,55 @@ public class AdminNewOrdersActivity extends AppCompatActivity {
         FirebaseRecyclerAdapter<Orders, OrdersViewHolder> adapter =
                 new FirebaseRecyclerAdapter<Orders, OrdersViewHolder>(options) {
                     @Override
-                    protected void onBindViewHolder(@NonNull OrdersViewHolder holder, int position, @NonNull Orders model)
+                    protected void onBindViewHolder(@NonNull OrdersViewHolder holder, final int position, @NonNull final Orders model)
                     {
                         holder.userName.setText("Name: " + model.getName());
                         holder.userPhoneNumber.setText("Phone: " + model.getPhone());
                         holder.userTotalPrice.setText("Total Price (VND): " + model.getTotalAmount());
                         holder.userShippingAddress.setText("Address, City:" + model.getAddress() + ", " + model.getCity());
                         holder.userDateTime.setText("Date Time: " + model.getDate() + ", " + model.getTime());
+
+                        holder.showAllProductsBtn.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                String uID = getRef(position).getKey();
+
+                                Intent intent = new Intent(AdminNewOrdersActivity.this,AdminUserProductsActivity.class);
+                                intent.putExtra("uid", uID);
+                                startActivity(intent);
+                            }
+                        });
+
+                        holder.itemView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                CharSequence options[] = new CharSequence[]
+                                        {
+                                                "Yes",
+                                                "No"
+                                        };
+
+                                AlertDialog.Builder builder = new AlertDialog.Builder(AdminNewOrdersActivity.this);
+                                builder.setTitle("Have You shipped this order products yet ?");
+
+                                builder.setItems(options, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        if (which == 0)
+                                        {
+                                            String uID = getRef(position).getKey();
+
+                                            RemoveOrder(uID);
+                                        }
+                                        else
+                                        {
+                                            finish();
+                                        }
+                                    }
+                                });
+                                builder.show();
+                            }
+                        });
                     }
 
                     @NonNull
@@ -69,6 +114,11 @@ public class AdminNewOrdersActivity extends AppCompatActivity {
 
         ordersList.setAdapter(adapter);
         adapter.startListening();
+    }
+
+    private void RemoveOrder(String uID)
+    {
+        ordersRef.child(uID).removeValue();
     }
 
 
